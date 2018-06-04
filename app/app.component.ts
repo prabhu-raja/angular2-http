d5032e83da4d0dcdb27fe024b1b51a3d2fe003ce
Prabhu <prabhu.raja@rci.rogers.com>
2018-06-04 17:49:02 -0400

    First Http Call 😗

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Http } from "@angular/http";
+import { User } from "./shared/models/user";
 @Component({
   selector: 'my-app',
-  styles: [`
-    
-  `],
   template: `
     <div class="jumbotron text-center">
       <h1>The App Lives!</h1>
-      <p>{{ message }}</p>
+      <div *ngIf="users">
+        <div *ngFor="let usr of users">
+          <h2>{{usr.first_name}}</h2>
+        </div>
+      </div>
     </div>
   `
 })
-export class AppComponent {
-  message = 'This is the sample message.';
+export class AppComponent implements OnInit {
+  users: User[];
+  constructor (private http: Http) {
+
+  }
+
+  ngOnInit() {
+    this.http.get('https://reqres.in/api/users')
+      .subscribe(data => {
+        this.users = data.json().data;
+      });
+  }
 }
